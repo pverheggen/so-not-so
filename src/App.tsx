62c369d70dc1eadd45d466fe10cc3ea97cf338f1
Paul Verheggen <paul.verheggen@gmail.com>
2024-01-20 22:48:24 -0800

    feat: test content

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,38 @@ import 'App.css';
 function App() {
   return (
     <Grid rows={6} columns={4} style={{ width: '98rem', height: '147rem' }}>
-      <Tile />
+      <Tile>
+        <div
+          style={{
+            gridRowStart: 2,
+            gridColumnStart: 3,
+            border: '1px solid #eee',
+          }}
+        ></div>
+        <div
+          style={{
+            gridRowStart: 3,
+            gridColumnStart: 3,
+            border: '1px solid #eee',
+          }}
+        ></div>
+      </Tile>
+      <Tile>
+        <div
+          style={{
+            gridRowStart: 1,
+            gridColumnStart: 1,
+            border: '1px solid #eee',
+          }}
+        ></div>
+        <div
+          style={{
+            gridRowStart: 4,
+            gridColumnStart: 4,
+            border: '1px solid #eee',
+          }}
+        ></div>
+      </Tile>
       <Tile />
       <Tile />
       <Tile />
