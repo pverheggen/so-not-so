8fecafafba4648be290537fce40547fdf6f315a0
Paul Verheggen <paul.verheggen@trustage.com>
2025-05-28 13:54:00 -0700

    optimize css

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
+import { optimizeCssModules } from 'vite-plugin-optimize-css-modules';
 import tsConfigPaths from 'vite-tsconfig-paths';
 import preact from '@preact/preset-vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact(), tsConfigPaths()],
+  plugins: [preact(), tsConfigPaths(), optimizeCssModules()],
 });
