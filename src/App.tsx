0bcf9c9261767d432ea18e52b8c0d9a4ca885a85
Paul Verheggen <paul.verheggen@gmail.com>
2025-06-02 05:33:16 -0700

    add app module css

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { Router } from 'components/Router';
 import { RouterProvider } from 'contexts';
+import classes from 'App.module.css';
 
 function App() {
   return (
-    <div className="app">
+    <div className={classes.app}>
       <RouterProvider>
         <Router />
       </RouterProvider>
